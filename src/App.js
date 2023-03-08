bea0e11fa3ca29ef5940433c972a9e775f027e6f
Preethika <ncpreethika30@gmail.com>
2023-03-08 13:45:41 +0530

    misa_v3

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import SideNavBar from './components/SideNavBar';
 import Header from './components/Header';
 import {Routes,Route, Navigate, Link} from 'react-router-dom'
 import SearchPage from './pages/SearchPage';
+import Login from "./pages/Login";
+import HomePage from "./pages/HomePage";
+import { useEffect, useState } from "react";
 
 function App() {
+  // const [navBarState, setNavBarState] = useState(false)
+
+  // useEffect(()=>{
+  //   window.location.pathname === "/" ? setNavBarState(false) : setNavBarState(true) 
+  //   console.log(window.location.pathname)
+  //   console.log(navBarState)
+  // })
+
   return (
     <>
-    <SideNavBar/>
-     <Header />
+    {window.location.pathname === "/" ? "" : <>{<SideNavBar/>} {<Header/>}</>}
+
+    {/* {navBarState ? <SideNavBar /> : "" } */}
+
+    {/* <SideNavBar/> */}
+     {/* <Header /> */}
     <Routes>
-     <Route path='/Search' element={<SearchPage/>}/>
+      <Route path='/' element={<Login/>}/>
+      <Route path='HomePage' element={<HomePage/>}/>
+      <Route path='Search' element={<SearchPage/>}/>
      </Routes>
     </>
   );
---
+++ src/components/LoginCard.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Style from './LoginCard.module.css';
+import { useNavigate } from "react-router-dom";
+
+function LoginCard() {
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    navigate("HomePage");
+    window.location.reload();
+  };
+
+  return (
+
+
+    <>
+    <div className ={Style.main}>
+      <p className={Style.sign} align="center">Sign in</p>
+      <form className={Style.form1} onSubmit={handleLogin}>
+        <input className = {Style.un} type="text" align="center" placeholder="Username"></input>
+          <input className = {Style.pass} type="password" align="center" placeholder="Password"></input>
+            <button className= {Style.submit} align="center" type="submit">Sign in</button>
+            <p className = {Style.forgot} align="center">
+              <a href="/">Forgot Password?</a></p>
+              </form>
+              </div>
+             
+    </>
+  )
+}
+
+export default LoginCard
