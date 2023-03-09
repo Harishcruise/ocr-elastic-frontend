f7b8121c43773ab4e7ee5f5b58e57f7798fe4356
Preethika <ncpreethika30@gmail.com>
2023-03-09 13:55:07 +0530

    misa_dv3

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -27,43 +27,43 @@ const upload = [
   {
     "name": "Jan",
     "uv": 4000,
-    "pv": 2400,
+    "Uploaded Rate": 2400,
     "amt": 2400
   },
   {
     "name": "March",
     "uv": 3000,
-    "pv": 1398,
+    "Uploaded Rate": 1398,
     "amt": 2210
   },
   {
     "name": "June",
     "uv": 2000,
-    "pv": 9800,
+    "Uploaded Rate": 9800,
     "amt": 2290
   },
   {
     "name": "Aug",
     "uv": 2780,
-    "pv": 3908,
+    "Uploaded Rate": 3908,
     "amt": 2000
   },
   {
     "name": "Sept",
     "uv": 1890,
-    "pv": 4800,
+    "Uploaded Rate": 4800,
     "amt": 2181
   },
   {
     "name": "Nov",
     "uv": 2390,
-    "pv": 3800,
+    "Uploaded Rate": 3800,
     "amt": 2500
   },
   {
     "name": "Dec",
     "uv": 3490,
-    "pv": 4300,
+    "Uploaded Rate": 4300,
     "amt": 2100
   }
 ]
@@ -131,16 +131,18 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
                                         <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                                     ))}
                                 </Pie>
+                                <Tooltip />
                             </PieChart>
 			</div>
 
       <div className={Style.Barchart}>
         <p>Uploaded Files based on file type</p>
       <BarChart width={450} height={196} margin={{ right:20}} data={files}>
-    <Bar dataKey="students" fill="green" />
-    <CartesianGrid stroke="#ccc" />
+    <Bar dataKey="students" fill="#FF69B4"></Bar>
+    {/* <CartesianGrid stroke="#ccc" /> */}
     <XAxis dataKey="name" />
     <YAxis />
+    <Tooltip cursor={false}/>
   </BarChart>
       </div>
       
@@ -148,38 +150,40 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
       <p>Monthly Overview</p>
      <AreaChart width={400} height={196} margin={{ right:20}} data={time}>
     <Area dataKey="students" fill="orange" stroke="orange" />
-    <CartesianGrid stroke="#ccc" />
+    {/* <CartesianGrid stroke="#ccc" /> */}
     <XAxis dataKey="name" />
     <YAxis/>
+    <Tooltip />
   </AreaChart>
      </div>
 
      
      <div className={Style.Linechart}>
-      <p>File Growth over Time</p>
+      <p>Growth over Time</p>
      <LineChart width={770} height={290} data={upload}
   margin={{right: 20, left: 20, bottom: 5 }}>
-  <CartesianGrid strokeDasharray="3 3" />
+  {/* <CartesianGrid strokeDasharray="3 3" /> */}
   <XAxis dataKey="name" />
   <YAxis />
   <Tooltip />
   <Legend />
-  <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+  <Line type="monotone" dataKey="Uploaded Rate" stroke="#8B008B" />
   {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
 </LineChart>
      </div>
 
      <div className={Style.Barchart1}>
-        <p>Uploaded Files based on file type</p>
+        <p>Recent Activity</p>
       <BarChart width={400} height={290} margin={{ right:20}} data={files}>
     <Bar dataKey="students" fill="#8884d8">
     {data.map((entry, index) => (
                                         <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                                     ))}
     </Bar>
-    <CartesianGrid stroke="#ccc" />
+    {/* <CartesianGrid stroke="#ccc" /> */}
     <XAxis dataKey="name" />
     <YAxis />
+    <Tooltip  cursor={false} />
   </BarChart>
       </div>
 
